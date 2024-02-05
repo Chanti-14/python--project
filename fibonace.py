f8d3238b7c4e6cfb6e0823b8dc663f71efaddef3
Chanti <romanempair.178@gmail.com>
2024-02-05 21:21:12 +0530

    modified new files

--- fibonace.py
+++ fibonace.py
@@ -2,7 +2,7 @@ limit=int(input("enter the number:"))
 new=0
 f1=1
 f2=2
-for i in tot("f1",     ,"f2"):
+for i in tot("f1"    "f2"):
     if limit>=0:
         new=new+f1
         f1=f2
--- quality_price.py
+++ quality_price.py
@@ -1,5 +1,7 @@
 def sim(qty,price):
     prod=qty*price
     return prod
+a=int(input("enter the number a:"))
+b=int(input("enter the number b:")) 
 total=sim(20,20)
 print(total)
