61292efadab9acf498f29d78e65813b5f1d1b80b
Chanti <romanempair.178@gmail.com>
2024-02-06 12:33:29 +0530

    Added new files in new folder

--- fibonace.py
+++ fibonace.py
@@ -2,12 +2,13 @@ limit=int(input("enter the number:"))
 new=0
 f1=1
 f2=2
-for i in tot("f1"    "f2"):
-    if limit>=0:
-        new=new+f1
-        f1=f2
-        f2=f1
-    print(limit)
+#for i in tot(f1,"    ",f2):
+while new<limit:
+    print(new,end=" ")
+    new=f1+f2
+    f1=f2
+    f2=new
+   
     
     
           
